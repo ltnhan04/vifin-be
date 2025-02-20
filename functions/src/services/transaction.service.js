6e801827defb3d3d9f1604b6a957d97e2bd32654
Luong Tu Nhan <tunhan281204@gmail.com>
2025-02-21 00:49:20 +0700

    feat: handle logic updated usage budget when cre/up

--- functions/src/services/transaction.service.js
+++ functions/src/services/transaction.service.js
@@ -82,23 +82,43 @@ class TransactionService {
   }) => {
     const transactionRef = db.collection("transactions").doc();
     const walletRef = db.collection("wallets").doc(wallet_id);
+    const budgetRef = await this.handleUpdateBudgetAmount(
+      wallet_id,
+      category_id
+    );
 
     await db.runTransaction(async (t) => {
       const walletDoc = await t.get(walletRef);
-      if (!walletDoc) {
+      if (!walletDoc.exists) {
         throw new ErrorHandler("Wallet does not exist", 404);
       }
-      const walletData = walletDoc.data();
-      let newBalance;
-      if (transaction_type === "expense") {
-        newBalance = Number(walletData.amount) - Number(amount);
-      } else if (transaction_type === "income") {
-        newBalance = Number(walletData.amount) + Number(amount);
-      } else {
-        throw new ErrorHandler("Invalid transaction type", 400);
+      let budgetDoc = null;
+      if (budgetRef && transaction_type === "expense") {
+        budgetDoc = await t.get(budgetRef);
       }
-      t.update(walletRef, { amount: newBalance, updatedAt: new Date() });
 
+      const walletData = walletDoc.data();
+      const newBalance = await this.calculateBalance(
+        transaction_type,
+        walletData.amount,
+        amount
+      );
+
+      let newUsage;
+      if (budgetDoc && budgetDoc.exists) {
+        const budgetData = budgetDoc.data();
+        newUsage = Number(budgetData.usage || 0) + Number(amount);
+      }
+
+      t.update(walletRef, { amount: newBalance, updatedAt: new Date() });
+      if (
+        budgetRef &&
+        transaction_type === "expense" &&
+        budgetDoc &&
+        budgetDoc.exists
+      ) {
+        t.update(budgetRef, { usage: newUsage, updatedAt: new Date() });
+      }
       const transactionData = {
         amount: amount || 0,
         customer_id: customer_id,
@@ -116,6 +136,7 @@ class TransactionService {
       _id: transactionRef.id,
     };
   };
+
   static updateTransactionWithWalletAdjustment = async (
     transactionId,
     newData
@@ -134,23 +155,86 @@ class TransactionService {
       if (!walletDoc.exists) {
         throw new ErrorHandler("Wallet does not exist", 404);
       }
-      const walletData = walletDoc.data();
+      let budgetDoc = null;
+      const budgetQuery = await t.get(
+        db
+          .collection("budgets")
+          .where("wallet_id", "==", oldTransaction.wallet_id)
+          .where("category_id", "==", oldTransaction.category_id)
+      );
+      if (!budgetQuery.empty) {
+        const bRef = db.collection("budgets").doc(budgetQuery.docs[0].id);
+        budgetDoc = await t.get(bRef);
+      }
 
-      const amountDiff = Number(newData.amount) - Number(oldTransaction.amount);
-      let newWalletAmount;
-      if (oldTransaction.transaction_type === "expense") {
-        newWalletAmount = Number(walletData.amount) - amountDiff;
-      } else if (oldTransaction.transaction_type === "income") {
-        newWalletAmount = Number(walletData.amount) + amountDiff;
-      } else {
-        throw new ErrorHandler("Invalid transaction type", 400);
+      const walletData = walletDoc.data();
+      const newWalletAmount = await this.handleUpdatedWalletAmount(
+        oldTransaction.transaction_type,
+        newData.amount,
+        walletData.amount,
+        oldTransaction.amount
+      );
+
+      let newUsage;
+      if (budgetDoc && budgetDoc.exists) {
+        let { usage = 0 } = budgetDoc.data();
+        if (oldTransaction.transaction_type === "expense") {
+          usage -= Number(oldTransaction.amount);
+        }
+        if (newData.transaction_type === "expense") {
+          usage += Number(newData.amount);
+        }
+        newUsage = usage;
       }
 
       t.update(walletRef, { amount: newWalletAmount, updatedAt: new Date() });
+      if (budgetDoc && budgetDoc.exists) {
+        const budgetRef = db.collection("budgets").doc(budgetQuery.docs[0].id);
+        t.update(budgetRef, { usage: newUsage, updatedAt: new Date() });
+      }
       t.update(transactionRef, { ...newData, updatedAt: new Date() });
     });
 
     return await this.getTransactionById(transactionId);
+  };
+
+  static calculateBalance = async (type, walletAmount, transactionAmount) => {
+    let newBalance;
+    if (type === "expense") {
+      newBalance = Number(walletAmount) - Number(transactionAmount);
+    } else if (type === "income") {
+      newBalance = Number(walletAmount) + Number(transactionAmount);
+    } else {
+      throw new ErrorHandler("Invalid transaction type", 400);
+    }
+    return newBalance;
+  };
+  static handleUpdatedWalletAmount = async (
+    type,
+    newAmount,
+    walletAmount,
+    transactionAmount
+  ) => {
+    const amountDiff = Number(newAmount) - Number(transactionAmount);
+    let newWalletAmount;
+    if (type === "expense") {
+      newWalletAmount = Number(walletAmount) - amountDiff;
+    } else if (type === "income") {
+      newWalletAmount = Number(walletAmount) + amountDiff;
+    } else {
+      throw new ErrorHandler("Invalid transaction type", 400);
+    }
+    return newWalletAmount;
+  };
+  static handleUpdateBudgetAmount = async (walletId, categoryId) => {
+    const budgetsDoc = await db
+      .collection("budgets")
+      .where("wallet_id", "==", walletId)
+      .where("category_id", "==", categoryId)
+      .get();
+    return budgetsDoc.empty
+      ? null
+      : db.collection("budgets").doc(budgetsDoc.docs[0].id);
   };
 }
 
