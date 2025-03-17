45e86acb2b2b29749cb94cc4a49c59c918f57215
LucHandsome <96497540+LucHandsome@users.noreply.github.com>
2025-03-17 22:48:09 +0700

    Lưu thay đổi trước khi pull

--- functions/src/validations/wallet.schema.js
+++ functions/src/validations/wallet.schema.js
@@ -1,15 +1,45 @@
 const Joi = require("joi");
 
 const createWalletSchema = Joi.object({
-  amount: Joi.number().min(0).required(),
-  currency_unit: Joi.string().valid("VND", "USD").required(),
-  wallet_name: Joi.string().min(3).max(50).trim().required(),
+  amount: Joi.number().min(0).required().messages({
+    "number.base": "Expected number, received string",
+    "number.min": "Amount must be a positive number",
+    "any.required": "amount is required"
+  }),
+  currency_unit: Joi.string().valid("VND", "USD").required().messages({
+    "any.only": "Currency must be either VND or USD",
+    "any.required": "currency_unit is required"
+  }),
+  wallet_name: Joi.string().min(3).max(50).trim().required().messages({
+    "string.min": "Wallet name must be at least 3 characters long",
+    "string.max": "Wallet name must not exceed 50 characters",
+    "any.required": "wallet_name is required"
+  }),
+  customer_id: Joi.string().required().messages({
+    "any.required": "customer_id is required"
+  }),
+  symbol: Joi.string().optional().allow(null, "").custom((value, helpers) => {
+    if (value && !/^https?:\/\/.+\.(jpg|jpeg|png|gif)$/.test(value)) {
+      return helpers.error("string.uri");
+    }
+    return value;
+  }).messages({
+    "string.uri": "Symbol must be a valid image URL (jpg, png, gif, jpeg)"
+  })
 }).unknown(true);
 
 const updateWalletSchema = Joi.object({
-  amount: Joi.number().min(0),
-  currency_unit: Joi.string().valid("VND", "USD"),
-  wallet_name: Joi.string().min(3).max(50).trim(),
+  amount: Joi.number().min(0).messages({
+    "number.base": "Expected number, received string",
+    "number.min": "Amount must be a positive number",
+  }),
+  currency_unit: Joi.string().valid("VND", "USD").messages({
+    "any.only": "Currency must be either VND or USD",
+  }),
+  wallet_name: Joi.string().min(3).max(50).trim().messages({
+    "string.min": "Wallet name must be at least 3 characters long",
+    "string.max": "Wallet name must not exceed 50 characters",
+  }),
 })
   .min(1)
   .unknown(true);
