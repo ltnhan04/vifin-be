5719d5a91b2e351e653e68a9a3f572a252de6e89
Luong Tu Nhan <tunhan281204@gmail.com>
2025-03-30 22:35:53 +0700

    fix: validate redundant customer_id

--- functions/src/jobs/budget-cron.js
+++ functions/src/jobs/budget-cron.js
@@ -1,17 +1,23 @@
 const cron = require("node-cron");
 const BudgetService = require("../services/budget.service");
 
-cron.schedule("0 0 * * *", async () => {
-  try {
-    const budgets = await BudgetService.getBudgets();
-    for (const budget of budgets) {
-      const isCompleted = await BudgetService.checkBudgetCompletion(budget);
-      if (budget.is_repeated && isCompleted) {
-        await BudgetService.handleRepeatBudget(budget);
+cron.schedule(
+  "0 0 * * *",
+  async () => {
+    try {
+      const budgets = await BudgetService.getBudgets();
+      for (const budget of budgets) {
+        const isCompleted = await BudgetService.checkBudgetCompletion(budget);
+        if (budget.is_repeated && isCompleted) {
+          await BudgetService.handleRepeatBudget(budget);
+        }
       }
+      console.log("Check budget auto repeat completed");
+    } catch (error) {
+      console.error(error);
     }
-    console.log("Check budget auto repeat completed");
-  } catch (error) {
-    console.error(error);
+  },
+  {
+    timezone: "Asia/Ho_Chi_Minh",
   }
-});
+);
--- functions/src/validations/wallet.schema.js
+++ functions/src/validations/wallet.schema.js
@@ -4,28 +4,29 @@ const createWalletSchema = Joi.object({
   amount: Joi.number().min(0).required().messages({
     "number.base": "Expected number, received string",
     "number.min": "Amount must be a positive number",
-    "any.required": "amount is required"
+    "any.required": "amount is required",
   }),
   currency_unit: Joi.string().valid("VND", "USD").required().messages({
     "any.only": "Currency must be either VND or USD",
-    "any.required": "currency_unit is required"
+    "any.required": "currency_unit is required",
   }),
   wallet_name: Joi.string().min(3).max(50).trim().required().messages({
     "string.min": "Wallet name must be at least 3 characters long",
     "string.max": "Wallet name must not exceed 50 characters",
-    "any.required": "wallet_name is required"
+    "any.required": "wallet_name is required",
   }),
-  customer_id: Joi.string().required().messages({
-    "any.required": "customer_id is required"
-  }),
-  symbol: Joi.string().optional().allow(null, "").custom((value, helpers) => {
-    if (value && !/^https?:\/\/.+\.(jpg|jpeg|png|gif)$/.test(value)) {
-      return helpers.error("string.uri");
-    }
-    return value;
-  }).messages({
-    "string.uri": "Symbol must be a valid image URL (jpg, png, gif, jpeg)"
-  })
+  symbol: Joi.string()
+    .optional()
+    .allow(null, "")
+    .custom((value, helpers) => {
+      if (value && !/^https?:\/\/.+\.(jpg|jpeg|png|gif)$/.test(value)) {
+        return helpers.error("string.uri");
+      }
+      return value;
+    })
+    .messages({
+      "string.uri": "Symbol must be a valid image URL (jpg, png, gif, jpeg)",
+    }),
 }).unknown(true);
 
 const updateWalletSchema = Joi.object({
